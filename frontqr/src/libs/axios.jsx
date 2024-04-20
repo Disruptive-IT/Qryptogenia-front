9b821125ef984de9dec79dc40a1538d594af13c2
Jobserd12 <114456323+Jobserd12@users.noreply.github.com.>
2024-04-19 19:04:59 -0500

    Ajustes en auth para login en back

--- frontqr/src/libs/axios.jsx
+++ frontqr/src/libs/axios.jsx
@@ -1,29 +1,10 @@
 import axios from 'axios';
 
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        let cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            let cookie = cookies[i].trim();
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
-let csrftoken = getCookie('csrftoken');
-const storedToken = localStorage.getItem('jwt_token');
-
 const instance = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "http://localhost:3000/api",
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
-        'X-CSRFToken': csrftoken,
     }
 });
 
--- frontqr/src/pages/Auth/LoginForm.jsx
+++ frontqr/src/pages/Auth/LoginForm.jsx
@@ -21,42 +21,54 @@ const LoginForm = () => {
       if (result.success) {
         resetForm();
       }
-    } catch (error) {
-      stopLoading();
     } finally {
       stopLoading();
     }
- };
+  };
 
   return (
     <Formik
-      initialValues={{ email: '', password: '', }}
+      initialValues={{ email: '', password: '', terms: false }}
       validationSchema={SchemaLoginValidate}
       onSubmit={handleSubmit}
     >
-      <section>
-        <Form className="authFormContainer my-20 m-auto">
-          <div className="formContainer">
-            <div className="inputsGroupsStart fullWidth">
-              <h1 className="authTittle"><span className='text-[#284B63]'>QR</span>yptogenia</h1>
-              <span className="fullWidth">Enter your details to access</span>
-              <div className="inputGroup relative">
-                <Field className="authInputs emailIcon" type="email" title="Email" name="email" placeholder="Email" maxLength="255" /><br />
-                <ErrorMessage name="email" className="errorMessaje absolute left-7" component='span' />
+      {({ values, errors, touched }) => (
+        <section>
+          <Form className="authFormContainer my-20 m-auto">
+            <div className="formContainer">
+              <div className="inputsGroupsStart fullWidth">
+                <h1 className="authTittle"><span className='text-[#284B63]'>QR</span>yptogenia</h1>
+                <span className="fullWidth">Enter your details to access</span>
+                <div className="inputGroup relative">
+                  <Field className="authInputs emailIcon" type="email" title="Email" name="email" placeholder="Email" maxLength="255" /><br />
+                  <ErrorMessage name="email" className="errorMessaje absolute left-7" component='span' />
+                </div>
+                <div className="inputGroup relative" style={{ marginBottom: 0 }}>
+                  <Field className="authInputs candado" type="password" title="Password" name="password" placeholder="Password" maxLength="64" /><br />
+                  <ErrorMessage name="password" className="errorMessaje absolute left-7" component='span' />
+                </div>
+                <div>
+                  <div className='mt-2 flex justify-start items-center'>
+                    <Field type="checkbox" name="remember" className="mr-2" />
+                    <label htmlFor="remember">Remember me</label>
+                  </div>
+                  <div className='mt-2 flex justify-start items-center'>
+                    <Field type="checkbox" name="terms" className="mr-2" />
+                    <label htmlFor="terms" className={`${touched.terms && !values.terms ? 'text-red-500' : ''}`}>I accept the Terms and Conditions</label>
+                  </div>
+                  {touched.terms && errors.terms && <div className="errorMessaje absolute left-7" style={{ color: 'red' }}>{errors.terms}</div>}
+                </div>
+                <Link className='text-[#103b79]' to="/forgotPassword">Forgot your password?</Link>
+                <SubmitButton text="Log in" />
+                <GoogleButton text="Log in with Google" />
               </div>
-              <div className="inputGroup relative">
-                <Field className="authInputs candado" type="password" title="Password" name="password" placeholder="Password" maxLength="64" /><br />
-                <ErrorMessage name="password" className="errorMessaje absolute left-7" component='span' />
-              </div>
-              <Link style={{ color: '#103b79' }} to="/forgotPassword">Forgot your password?</Link>
-              <SubmitButton text="Log in" />
-              <GoogleButton text="Log in with Google" />
+              <img src={logo} className="elLogoRigth" alt="" />
+              <IconsRight />
             </div>
-            <img src={logo} className="elLogoRigth" alt="" />
-            <IconsRight />
-          </div>
-        </Form>
-      </section>
+
+          </Form>
+        </section>
+      )}
     </Formik>
   );
 }
