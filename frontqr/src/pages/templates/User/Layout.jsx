7a7f4992acdd3e9efdd70e899cc7ca04ba98938a
Jobserd12 <114456323+Jobserd12@users.noreply.github.com.>
2024-04-25 08:57:51 -0500

    Proteccion de rutas y login fallido

--- frontqr/src/pages/templates/User/Layout.jsx
+++ frontqr/src/pages/templates/User/Layout.jsx
@@ -1,11 +1,12 @@
+import { Outlet } from 'react-router-dom';
 import Navbar from './Header';
 
-export default function LayoutUser({children}) {
+export default function LayoutUser() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-grow">
-        {children}
+        <Outlet/>
       </main>
     </div>
   );
