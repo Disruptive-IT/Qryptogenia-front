901c3389532deaa806233f2111f16e17e067855b
Jobserd12 <114456323+Jobserd12@users.noreply.github.com.>
2024-05-27 13:01:00 -0500

    Merge

--- frontqr/src/components/Layout/qrContent/customQr/frame/index.jsx
+++ frontqr/src/components/Layout/qrContent/customQr/frame/index.jsx
@@ -14,24 +14,10 @@ const Frame = () => {
         setQrText(event.target.value);
     };
 
-  
-
-    const handleFontSizeChange = (event) => {
-        setFontSize(event.target.value);
-    };
-
     return (
         <>
             <div className="flex space-x-4 items-center">
                 <InputText label="Escribir el texto" variant="filled" fullWidth onChange={handleInputChange} />
-                <div className='flex flex-col gap-2'>
-                    <Input
-                        type="number"
-                        inputProps={{ min: "10", max: "25", step: "1" }}
-                        onChange={handleFontSizeChange}
-                        style={{ width: '50px' }}
-                    />
-                </div>
             </div>
             <span className='text-xs text-gray-500'>*El texto tiene un limite de 20 caracteres</span>
             <ScrollableMain />
