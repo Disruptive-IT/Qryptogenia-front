3031e07eb27e6501a4bf9ecfe8c2619034cd0a3b
DyPhox <litrex2209@gmail.com>
2024-06-04 23:18:14 -0500

    ajuste tabla

--- frontqr/src/pages/tableqr/QRCodeRow.jsx
+++ frontqr/src/pages/tableqr/QRCodeRow.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { FaDownload, FaShareAlt } from 'react-icons/fa';
+import { MdOutlineEdit } from "react-icons/md";
 import QRCode from 'qrcode.react';
 
 const QRCodeRow = ({ code }) => {
     return (
         <tr className="hover:bg-gray-100 cursor-pointer">
-            <td className="px-6 py-4 whitespace-nowrap">
-                <input type="checkbox" className="form-checkbox h-4 w-4 text-blue-600" />
-            </td>
             <td className="px-6 py-4 whitespace-nowrap">
                 <QRCode value={code.name} size={32} />
             </td>
@@ -20,9 +18,9 @@ const QRCodeRow = ({ code }) => {
                 </span>
             </td>
             <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{code.date}</td>
-            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium flex">
                 <FaDownload className="text-gray-500 hover:text-gray-700 cursor-pointer mr-3" />
-                <FaShareAlt className="text-gray-500 hover:text-gray-700 cursor-pointer" />
+                <MdOutlineEdit className="text-gray-500 hover:text-gray-700 cursor-pointer" />
             </td>
         </tr>
     );
--- frontqr/src/pages/tableqr/QRTable.jsx
+++ frontqr/src/pages/tableqr/QRTable.jsx
@@ -7,7 +7,6 @@ const QRTable = ({ qrCodes }) => {
             <table className="min-w-full divide-y divide-gray-200">
                 <thead className="bg-gray-50">
                     <tr>
-                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Select</th>
                         <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">QR Code</th>
                         <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">QR Code Name</th>
                         <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">QR Code Type</th>
