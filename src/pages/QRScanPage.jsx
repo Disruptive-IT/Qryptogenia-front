bdc14b6ec61223a5c0ca252589982d6edb0d33d5
Jobserd12 <114456323+Jobserd12@users.noreply.github.com.>
2024-07-22 14:31:59 -0500

    Verfication router complte

--- src/pages/QRScanPage.jsx
+++ src/pages/QRScanPage.jsx
@@ -11,19 +11,21 @@ import { SocialButtonM } from '../components/Layout/qrContent/socialMedia/social
  * @return: Retorna los datos obtenidos de la petición, incluyendo posibles errores y resultados exitosos
 **/
 
-const musicData = {
-    selectedOptions: [
-        { value: 'spotify', url: 'https://spotify.com' },
-        { value: 'youtube', url: 'https://youtube.com' },
-    ],
-    backgroundColor: 'linear-gradient(180deg, #fff 0.00%,rgb(50, 52, 53) 100.00%)',
-    borderColor: '#000000',
-    image: 'https://cdn-icons-png.flaticon.com/512/25/25231.png',
-    titleColor: '#000',
-    descriptionColor: '#000',
-    title: 'Título',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-};
+// const musicData = {
+//     selectedOptions: [
+//         { value: 'spotify', url: 'https://spotify.com' },
+//         { value: 'youtube', url: 'https://youtube.com' },
+//     ],
+//     backgroundColor: 'linear-gradient(180deg, #fff 0.00%,rgb(50, 52, 53) 100.00%)',
+//     borderColor: '#000000',
+//     image: 'https://cdn-icons-png.flaticon.com/512/25/25231.png',
+//     titleColor: '#000',
+//     descriptionColor: '#000',
+//     title: 'Título',
+//     description: '.',
+// };
+
+
 
 const QRScanPage = () => {
     const [searchParams] = useSearchParams();
@@ -35,8 +37,9 @@ const QRScanPage = () => {
     const fetchData = async () => {
         try {
             const res = await axios.get(`/verify-qr?q=${qrId}`);
-            console.log(res.data)
-            setQrData(musicData);
+            console.log(res.data.QrPreview)
+            setQrData(res.data.QrPreview);
+            // setQrData(musicData);
             setLoading(false);
         } catch (err) {
             setError(err.response?.data?.msg || 'Error de verificación de QR');
@@ -69,22 +72,25 @@ const QRScanPage = () => {
         );
     }
 
-    const dataBtns = Array.isArray(qrData.selectedOptions) ? qrData.selectedOptions.map(option => ({
-        name: option.value.charAt(0).toUpperCase() + option.value.slice(1),
+    // Acá se debe de tomar correctamente el nombre del enlace, dicho nombre se usa para tomar los iconos.
+    //! Al momento de guardar las opciones de seleccion de la preview, se debes de guardar de manera que se pueda tomar el "value" para tomar el icono de react-icons y presentar el texto
+    const dataBtns = Array.isArray(qrData.SelectOptions) ? qrData.SelectOptions.map(option => ({
+        name: option.value.split(' ')[0].charAt(0).toUpperCase() + option.value.split(' ')[0].slice(1),
         url: option.url
     })) : [];
 
+    
     return (
         <div className='flex items-center justify-center min-h-screen'>
-            <div className='flex flex-col min-h-screen w-full items-center justify-center' style={{ background: qrData.backgroundColor }}>
+            <div className='flex flex-col min-h-screen w-full items-center justify-center' style={{ background: qrData.backgroudColor }}>
                 <div className='flex flex-col items-center mt-10 md:mt-28 bg-white rounded-2xl w-[90%] sm:w-[400px] md:w-[600px] min-h-[400px] max-h-[600px] p-6 shadow-lg' style={{ background: qrData.boxColor }}>
-                    <div className='relative bg-white rounded-2xl -mt-14 border-4 shadow-md p-1 transition-shadow hover:shadow-xl' style={{ borderColor: qrData.borderColor }}>
-                        <img className='w-20' src={qrData.image} alt="" />
+                    <div className='relative bg-white rounded-2xl -mt-14 border-4 shadow-md p-1 transition-shadow hover:shadow-xl' style={{ borderColor: qrData.borderImg }}>
+                        <img className='w-20' src={qrData.image} alt="imgBoxBackgroud" />
                     </div>
                     <div className="mt-4 mb-2 w-[90%] text-center">
                         <h1
                             className="text-2xl mb-2 font-bold"
-                            style={{ color: qrData.titleColor }}
+                            style={{ color: qrData.colorTitle }}
                         >
                             {qrData.title}
                         </h1>
@@ -101,5 +107,9 @@ const QRScanPage = () => {
         </div>
     );
 };
+
+//? Como tomar las img
+//? Como tomar los iconos
+//! Limitar entradas 
 
 export default QRScanPage;
