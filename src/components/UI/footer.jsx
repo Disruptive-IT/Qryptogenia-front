007d6e81ae3ccc1dd3e77672fb0a71c731f960d0
Daniel Salazar <jdps106@hotmail.com>
2024-07-29 16:22:31 -0500

    se le dan estilos y se pone responsive el footer del landing

--- src/components/UI/footer.jsx
+++ src/components/UI/footer.jsx
@@ -1,28 +1,46 @@
 import React from "react";
 import { FaFacebookSquare , FaInstagramSquare, FaTwitterSquare } from 'react-icons/fa';
 import disruptive from "../../assets/imgs/disruptive/disruptive.png";
+import { FaLinkedin, FaSquareXTwitter  } from "react-icons/fa6";
 
 
 export const Footer = () => {
+  const gradientStyle = {
+    background: 'linear-gradient(45deg, #f58529, #dd2a7b, #8134af, #515bd4)',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+    backgroundClip: 'text',
+    textFillColor: 'transparent',
+  };
 
     const socialMedia = [
       {
-        icon: FaFacebookSquare,
-        link: 'https://www.facebook.com',
+        icon: <FaFacebookSquare className="text-[#0865fe] w-6 h-6" title="Facebook" />,
+        link: 'https://www.facebook.com/disruptive.devops?locale=es_LA',
+        color: '#0865fe'
       },
       {
-        icon: FaTwitterSquare,
-        link: 'https://www.twitter.com',
+        icon: <FaLinkedin className="text-[#0b65c3] w-6 h-6" title="Linkedin" />,
+        link: 'https://www.linkedin.com/company/disruptive-information-technologies/',
+        color: '#0b65c3'
       },
       {
-        icon: FaInstagramSquare,
-        link: 'https://www.instagram.com',
+        icon: <FaInstagramSquare className="text-[#c32a5f] w-6 h-6" title="Instagram" />,
+        link: 'https://www.instagram.com/disruptive.info/',
+        color: '#0865fe'
+
+      },
+      {
+        icon: <FaSquareXTwitter className="text-black w-6 h-6" title="Instagram" />,
+        link: 'https://x.com/DisruptiveITDev',
+        color: '#0865fe'
+
       },
     ]
 
     
   return (
-    <footer className="">
+    <footer className="mt-10">
       <svg
         viewBox="0 -20 700 110"
         width="100%"
@@ -39,38 +57,38 @@ export const Footer = () => {
           fill="#cbd5e1"
         />
       </svg>
-      <div className="flex justify-between bg-slate-300 w-full px-6">
+      <div className="flex justify-between flex-wrap bg-slate-300 w-full px-6">
 
-        <div className="flex flex-col w-2/6 text-slate-500">
+        <div className="cursor-default flex flex-col items-center md:items-start justify-center w-full md:w-2/6 text-slate-500">
           <p><strong>Contact us</strong></p>
           <p><strong>Email:</strong> disruptive.devops@gmail.com</p>
-          <p><strong>Phone:</strong> +1 (555) 555-5555</p>
-          <p>Terms of use</p>
-          <p>Privacy policy</p>
+          <p><strong>Phone:</strong> +57 324 2167567</p>
         </div>
 
-        <div className="flex flex-col items-center justify-center text-center w-2/6">
-            <span className=" text-slate-500">Created By</span>
+        <div className="flex flex-col items-center justify-center text-center w-full md:w-2/6">
+            <span className="cursor-default text-slate-500"><strong>Developed By</strong></span>
             <img src={disruptive} alt="disruptive" className="w-36 h-36"/>
             
         </div>
 
-        <div className="flex flex-col w-2/6 items-end text-slate-500">
-          <p>Social media:  </p>
+        <div className="flex flex-col items-center  gap-2 w-full md:w-2/6 md:items-end text-slate-500">
           <div className="flex gap-1 flex-nowrap">
+            
           {socialMedia.map((item, index) => (
               <a href={item.link} target="_blank" rel="noreferrer" key={index}>
-              <div className="flex items-center justify-center">
-                <item.icon className="w-6 h-6" />
+              <div className="flex items-center justify-center transition-all hover:scale-110 duration-300">
+                {item.icon}
               </div>
             </a>
           ))}
           </div>
+          
+          <p className="font-bold cursor-pointer">Terms of use</p>
+          <p className="font-bold cursor-pointer">Privacy policy</p>
         </div>
         
       </div>
-      <div className="w-full flex justify-center bg-slate-300 py-2 ">
-
+      <div className="w-full flex justify-center bg-slate-300 mb-[55px] md:mb-0 md:py-3  ">
         <p className="text-slate-500 font-bold">© 2024 All rights reserved</p>
       </div>
     </footer>
