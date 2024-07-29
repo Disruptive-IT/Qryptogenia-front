a95b15394a6e96db4f1580b6c57741147b1f4460
Daniel Salazar <jdps106@hotmail.com>
2024-07-29 16:48:09 -0500

    se implementra footer en el layout

--- src/pages/templates/Home/Layout.jsx
+++ src/pages/templates/Home/Layout.jsx
@@ -1,3 +1,4 @@
+import { Footer } from '../../../components/UI/footer';
 import StepperQr from '../../../components/UI/utils/stepper';
 import Navbar from './Header';
 import { Outlet, useLocation } from 'react-router-dom';
@@ -6,7 +7,7 @@ function LayoutHome() {
     const location = useLocation();
 
     return (
-        <div className="flex flex-col min-h-screen relative">
+        <div className="flex flex-col min-h-screen relative ">
             <Navbar />
             {location.pathname !== '/login' && location.pathname !== '/register'  && location.pathname !== '/forgotPassword'  && (
                 <div className='mt-32'>
@@ -16,6 +17,7 @@ function LayoutHome() {
             <main className="flex-grow mt-8">
                 <Outlet />
             </main>
+            <Footer/>
         </div>
     );
 }
