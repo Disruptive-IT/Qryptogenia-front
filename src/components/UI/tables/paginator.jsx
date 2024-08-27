6b79d6f654da544bab4d06ff111c9601be8e99a7
Salazar106 <jdps106@hotmail.com>
2024-08-27 16:32:06 -0500

    se realizan ajustes al paginador en estilos

--- src/components/UI/tables/paginator.jsx
+++ src/components/UI/tables/paginator.jsx
@@ -4,23 +4,23 @@ import { useTranslation } from 'react-i18next';
 const Paginator = ({ currentPage, totalPages, onPageChange }) => {
   const { t } = useTranslation();
   return (
-    <div className="flex flex-col sm:flex-row justify-between items-center mt-4">
+    <div className="flex flex-col sm:flex-row justify-center items-center  gap-3 mt-4">
       <button
         onClick={() => onPageChange(currentPage - 1)}
         disabled={currentPage === 1}
-        className="px-4 py-2 mb-2 sm:mb-0 sm:mr-2 bg-light-blue hover:bg-dark-blue text-white rounded disabled:bg-gray-300"
+        className="bg-light-blue w-8 h-8 rounded-full cursor-pointer text-white transition-all duration-200 hover:bg-dark-blue hover:scale-110"
       >
-        {t("Previous")}
+        {"<"}
       </button>
-      <span className="text-xs sm:text-sm">
-      {t("Page")} {currentPage} of {totalPages}
-      </span>
+      <p className="text-xs sm:text-sm text-slate-600">
+      {t("Page")} <span className='font-bold text-md '>{currentPage}</span> of {totalPages}
+      </p>
       <button
         onClick={() => onPageChange(currentPage + 1)}
         disabled={currentPage >= totalPages}
-        className="px-4 py-2 mt-2 sm:mt-0 sm:ml-2 bg-light-blue hover:bg-dark-blue text-white rounded disabled:bg-gray-300"
+        className="bg-light-blue w-8 h-8 rounded-full cursor-pointer text-white transition-all duration-200 hover:bg-dark-blue hover:scale-110"
       >
-        {t("Next")}
+        {">"}
       </button>
     </div>
   );
