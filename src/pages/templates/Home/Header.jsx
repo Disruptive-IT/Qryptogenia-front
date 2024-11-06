d5500f3d76e2d121abcd55aa3419dc45592a2e5e
CristianRueda0721 <131409111+CristianRueda0721@users.noreply.github.com>
2024-11-06 13:41:55 -0500

    FAQ

--- src/pages/templates/Home/Header.jsx
+++ src/pages/templates/Home/Header.jsx
@@ -28,7 +28,7 @@ function Navbar() {
                         <CustomLink to="/" isActive={isActive('/')}>{t('Home')}</CustomLink>
                         <CustomLink to="#" isActive={isActive('/about')}>{t('About us')}</CustomLink>
                         <CustomLink to="/pricings" isActive={isActive('/pricings')}>{t('Plans')}</CustomLink>
-                        <CustomLink to="#" isActive={isActive('/faq')}>FAQ</CustomLink>
+                        <CustomLink to="#" isActive={isActive('/faq')}>{t("FAQ")}</CustomLink>
                     </ul>
                 </nav>
 
