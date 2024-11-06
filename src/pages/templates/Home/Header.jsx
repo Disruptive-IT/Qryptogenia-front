f0afca9785235e41139eff46d3b4f79fe8ca8533
CristianRueda0721 <131409111+CristianRueda0721@users.noreply.github.com>
2024-11-06 13:36:10 -0500

    Logo

--- src/pages/templates/Home/Header.jsx
+++ src/pages/templates/Home/Header.jsx
@@ -7,7 +7,6 @@ import { useAuth } from '../../../hooks/useAuth';
 import UserProfileMenu from '../../../components/Admin/Profile';
 import { useTranslation } from 'react-i18next';
 import LenguageSelector from './../../../components/UI/lenguage/lenguageSelector'
-const logo = 'Logo.png';
 
 function Navbar() {
     const location = useLocation();
@@ -21,8 +20,8 @@ function Navbar() {
         <header className="relative md:top-4 inset-x-0 z-50">
             <div className="relative h-20 flex w-full p-3 mx-auto bg-white md:rounded-lg items-center justify-between shadow-lg lg:w-[94%] ">
                 <Link to="/" className="text-black hover:text-black/70 items-center inline-flex font-bold ml-2 text-2xl w-2/6" title="Inicio">
-                    <img className='w-[60px]' src={logo} alt="Qryptogenia" />
-                    <span className='text-dark-blue ml-2 font-bold'>QR</span>yptogenia
+                <img className='w-[60px]' src="/Logo.png" alt="Qryptogenia" />
+                <span className='text-dark-blue ml-2 font-bold'>QR</span>yptogenia
                 </Link>
 
                 <nav className="md:flex hidden w-2/6 justify-center">
