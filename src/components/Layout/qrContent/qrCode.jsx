fba25a9a6c13b421ee1e1a7d648f303c4cb2b43b
jhonyx <jnba0529@gmail.com>
2024-11-20 09:11:46 -0500

    test four scan

--- src/components/Layout/qrContent/qrCode.jsx
+++ src/components/Layout/qrContent/qrCode.jsx
@@ -197,7 +197,7 @@ const QR = ({ uniqueKey }) => {
                 qrCode.current = new QRCodeStyling({
                     width: 1000,
                     height: 1000,
-                    data: `https://apiqryptogenia.disruptiveinfotech.com/api/qr/scan/${uniqueKey}`, //modificar aqui para crear url directa
+                    data: `https://apiqryptogenia.disruptiveinfotech.com/api/scan/${uniqueKey}` || `${import.meta.env.VITE_BASE_BACK_URL}/scan/${uniqueKey}`, //modificar aqui para crear url directa
                     dotsOptions: {
                         color: qrProps.dotsColor,
                         type: qrProps.dotsType || 'rounded'
@@ -224,7 +224,7 @@ const QR = ({ uniqueKey }) => {
                 qrCode.current.append(qrRef.current);
             } else {
                 qrCode.current.update({
-                    data:`${import.meta.env.VITE_BASE_BACK_URL}/qr/scan/${uniqueKey}` || `https://apiqryptogenia.disruptiveinfotech.com/api/qr/scan/${uniqueKey}`, //modificar aqui para crear url directa
+                    data:`https://apiqryptogenia.disruptiveinfotech.com/api/scan/${uniqueKey}` || `${import.meta.env.VITE_BASE_BACK_URL}/scan/${uniqueKey}`, //modificar aqui para crear url directa
                     margin: 10,
                     backgroundOptions: {
                         color: "transparent",
